8708d9a81bae470bd3f5e52703167927a577e3c9
imkoolya <imkoolya@gmail.com>
2023-12-25 16:23:22 +0300

    Добавьте файлы проекта.

---
+++ module-5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace module5 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Dishes) User;
+            User.Dishes = new string[5];
+
+            Console.WriteLine("Введите имя пользователя");
+            User.Name = Console.ReadLine();
+
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine($"Введите любимое блюдо номер {i+1}");
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+        }
+    }
+}
