d9baed24c3a91ec17e5c4ff0f00eb3ffbf438df9
imkoolya <imkoolya@gmail.com>
2023-12-31 15:46:28 +0300

    Итоговый проект 5.6.1 (HW-03)

--- module-5/Program.cs
+++ module-5/Program.cs
@@ -1,27 +1,86 @@
 ﻿using System;
 
-class MainClass
+class Program
 {
-    public static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine(PowerUp(2, 3));
+        var user = GetUserDetails();
+        DisplayUserDetails(user);
     }
-    public static int PowerUp(int N, byte pow)
+
+    static (string firstName, string lastName, int age, bool hasPets, string[] petNames, string[] favoriteColors) GetUserDetails()
     {
-        if (pow == 0)
+        Console.Write("Введите ваше имя: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Введите вашу фамилию: ");
+        string lastName = Console.ReadLine();
+
+        int age = GetValidNumberFromConsole("Введите ваш возраст: ");
+
+        Console.Write("Есть ли у вас питомец? (да/нет): ");
+        bool hasPets = Console.ReadLine().ToLower() == "да";
+
+        string[] petNames = null;
+        if (hasPets)
         {
-            return 1;
+            int numPets = GetValidNumberFromConsole("Сколько у вас питомцев? ");
+            petNames = GetPetNames(numPets);
         }
-        else
+
+        int numColors = GetValidNumberFromConsole("Сколько у вас любимых цветов? ");
+        string[] favoriteColors = GetFavoriteColors(numColors);
+
+        return (firstName, lastName, age, hasPets, petNames, favoriteColors);
+    }
+
+    static int GetValidNumberFromConsole(string prompt)
+    {
+        int number;
+        while (true)
         {
-            if (pow == 1)
+            Console.Write(prompt);
+            if (int.TryParse(Console.ReadLine(), out number) && number > 0)
             {
-                return N;
+                return number;
             }
-            else
-            {
-                return N * PowerUp(N, --pow);
-            }
+            Console.WriteLine("Пожалуйста, введите корректное число.");
         }
+    }
+
+    static string[] GetPetNames(int numPets)
+    {
+        string[] petNames = new string[numPets];
+        for (int i = 0; i < numPets; i++)
+        {
+            Console.Write($"Введите имя {i + 1} питомца: ");
+            petNames[i] = Console.ReadLine();
+        }
+        return petNames;
+    }
+
+    static string[] GetFavoriteColors(int numColors)
+    {
+        string[] favoriteColors = new string[numColors];
+        for (int i = 0; i < numColors; i++)
+        {
+            Console.Write($"Введите ваш любимый цвет №{i + 1}: ");
+            favoriteColors[i] = Console.ReadLine();
+        }
+        return favoriteColors;
+    }
+
+    static void DisplayUserDetails((string firstName, string lastName, int age, bool hasPets, string[] petNames, string[] favoriteColors) user)
+    {
+        Console.WriteLine("\nВаши данные:");
+        Console.WriteLine($"Имя: {user.firstName}");
+        Console.WriteLine($"Фамилия: {user.lastName}");
+        Console.WriteLine($"Возраст: {user.age}");
+        Console.WriteLine($"Наличие питомца: {(user.hasPets ? "Да" : "Нет")}");
+        if (user.hasPets)
+        {
+            Console.WriteLine("Имена питомцев: " + string.Join(", ", user.petNames));
+        }
+        Console.WriteLine("Любимые цвета: " + string.Join(", ", user.favoriteColors));
     }
 }
